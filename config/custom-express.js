b661d2f7b8d5e1a9bce11d7d10ad30d0e5cf9390
leolousa <leolousa@hotmail.com>
2018-11-26 10:55:06 -0200

    Inserindo mais dados no post

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,8 +1,13 @@
 var express = require('express');       // Carrega o módulo do Express
 var consign = require('consign');       // Carrega o módulo do Consign
+var bodyParser = require('body-parser');// Carrega o módulo body-parser
+
 
 module.exports = function() {
   var app = express();
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
 
   consign()
     .include('resources')
--- resources/pagamentos.js
+++ resources/pagamentos.js
@@ -1,8 +1,18 @@
-
 module.exports = function(app) {
+  
   app.get('/pagamentos', function(req, res) {
     console.log('Recebida requisição!');
     res.send('OK.');
+  });
+
+  app.post('/pagamentos/pagamento', function(req, res) {
+    var pagamento = req.body;
+    console.log('Processando a rewuisição de um novo pagamento!');
+
+    pagamento.status = 'CRIADO';
+    pagamento.data = new Date();
+    
+    res.send(pagamento);
   });
 
 }
