941e0b0351de7de6b87a65fb9b2a39fc9f74af26
leolousa <leolousa@hotmail.com>
2018-11-27 10:53:51 -0200

    Retornando status code correto e location criada

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,13 +1,14 @@
-var express = require('express');       // Carrega o módulo do Express
-var consign = require('consign');       // Carrega o módulo do Consign
-var bodyParser = require('body-parser');// Carrega o módulo body-parser
-
+var express = require('express');             // Carrega o módulo do Express
+var consign = require('consign');             // Carrega o módulo do Consign
+var bodyParser = require('body-parser');      // Carrega o módulo body-parser
+var validator = require('express-validator'); // Carrega o módulo do express-validator
 
 module.exports = function() {
   var app = express();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  app.use(validator());
 
   consign()
     .include('resources')               // Inclui a pasta 'resources'
--- resources/pagamentos.js
+++ resources/pagamentos.js
@@ -6,19 +6,41 @@ module.exports = function(app) {
   });
 
   app.post('/pagamentos/pagamento', function(req, res) {
+
+    // Definindo as validações
+    req.assert('forma_de_pagamento', 'Forma de pagamento é obrigatoria!').notEmpty();
+    req.assert('valor', 'Valor é obrigatorio e deve ser decimal!').notEmpty().isFloat();
+
+    // Verifica se há erros de validação
+    var erros = req.validationErrors();
+
+    if (erros) {
+      console.log('erros de validação encontrados!');
+      res.status(400).send(erros);
+      return
+    }
+
     var pagamento = req.body;
     console.log('Processando a rewuisição de um novo pagamento!');
 
     pagamento.status = 'CRIADO';
     pagamento.data = new Date();
 
-    var conn = app.persistencia.connectionFactory();            // Acessa a pasta que tem o módulo de conexão com o BD
+    var conn = app.persistencia.connectionFactory();                // Acessa a pasta que tem o módulo de conexão com o BD
     var pagamentoDAO = new app.persistencia.PagamentoDAO(conn);
 
     pagamentoDAO.salva(pagamento, function(erro, resultado) {
-      console.log('Pagamento criado');
-      res.json(pagamento);
+      if(erro) {
+        console.log('Erro ao inserir no banco: ' + erro);
+        res.status(500).send(erro);
+      } else {
+        console.log('Pagamento criado');
+        res.location('/pagamentos/pagamento/' + resultado.insertId);  // Envia a URL do recurso criado no resultado
+        res.status(201).json(pagamento);
+      }
     });
+
+
 
   });
 
